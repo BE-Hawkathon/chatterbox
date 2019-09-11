737564c21ab2e5386caad92d50fc5e2f6ed9dd26
mayankagarwal <mayank.agarwal@birdeye.com>
2019-09-11 16:42:41 +0530

    Intital setup

---
+++ src/main/java/org/birdeye/chatterbox/ChatterBoxApplication.java
@@ -0,0 +1,16 @@
+package org.birdeye.chatterbox;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatterBoxApplication
+{
+    public static void main(String[] args)
+    {
+        SpringApplication app = new SpringApplication(ChatterBoxApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
+    }
+}
